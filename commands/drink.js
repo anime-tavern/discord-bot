d953312494563b077341e79acb33b79a1f5d349a
nox7 <17110935+nox7@users.noreply.github.com>
2020-10-18 18:10:56 -0500

    Create drink.js

---
+++ commands/drink.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+
+module.exports = {
+	fire:function(msgContent){
+		const embed = new Discord.MessageEmbed();
+		embed.setTitle("Drink up!");
+		embed.attachFiles([
+			"./images/drink-up-1.png"
+		]);
+		embed.setImage("attachment://drink-up-1.png");
+
+		return embed;
+	},
+
+	// The matrix of strings to use to determine if this command matches the msgContent
+	messageMatrix:[
+		["i", "need", "a", "drink"],
+		['with', 'a', 'drink'],
+		['set','up','a','drink'],
+		['serve','me','drink'],
+		['serve','us',/drinks*/],
+		['we','need',/drinks*/],
+		['we','need','a','drink'],
+		["we're","thirsty"],
+		[/i'*m/,"thirsty"]
+	]
+};
